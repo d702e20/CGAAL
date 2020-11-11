fb30b34d7d2a95c16335acffa862425a7bb657e6
cogitantium <falke.cs@gmail.com>
2020-11-11 08:52:22 +0100

    Initial add of parser

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate num_cpus;
 use crate::common::Edges;
 use std::collections::hash_map::RandomState;
 use std::collections::HashSet;
+use clap::{App, Arg};
 
 mod atl;
 mod com;
@@ -20,5 +21,19 @@ impl edg::ExtendedDependencyGraph<i32> for EmptyGraph {
 }
 
 fn main() {
+    let args = App::new("OnTheFlyATL")
+        .version("0.1.0")
+        .author("d702e20 <d702e20@cs.aau.dk>")
+        .arg(Arg::with_name("prop")
+            .short("p")
+            .long("prop")
+            .env("PROPOSITION")
+            .help("The proposition to check for")
+        ).arg(Arg::with_name("input_file")
+        .short("i")
+        .long("input")
+        .env("INPUT_FILE")
+        .help("The input file to generate model from")).get_matches();
+
     edg::distributed_certain_zero(EmptyGraph {}, 0, num_cpus::get() as u64);
 }
