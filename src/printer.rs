7fa654761aa74cbf65441d80c9eb5165470a9be2
Jacob Jenner Rasmussen <jacob@jener.dk>
2020-11-13 15:10:26 +0100

    Fixes vertices not being explored

--- src/printer.rs
+++ src/printer.rs
@@ -1,6 +1,5 @@
 use crate::common::Edges;
 use crate::edg::{ExtendedDependencyGraph, Vertex};
-use serde::export::fmt::Display;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashSet, VecDeque};
 use std::fmt::{Debug, Display};
@@ -28,68 +27,72 @@ pub(crate) fn print_graph<V: Vertex, G: ExtendedDependencyGraph<V>, W: Write>(
     output.write("digraph edg {\n".as_bytes())?;
 
     let mut visited: HashSet<V> = HashSet::new();
-    let mut queue = VecDeque::new();
+    let mut queue: VecDeque<V> = VecDeque::new();
     let mut hyper_idx = 0usize;
 
-    for edge in graph.succ(&v0) {
-        queue.push_back(edge);
-    }
+    queue.push_back(v0.clone());
+
     print_vertex(&v0, &mut output)?;
     visited.insert(v0);
 
     while !queue.is_empty() {
-        let edge = queue
+        let vertex = queue
             .pop_front()
             .expect("non-empty queue failed to pop element");
 
-        // TODO maybe print labels on edges
-        match edge {
-            Edges::HYPER(hyper) => {
-                if hyper.targets.is_empty() {
-                    output.write(format!("v{} -> ∅;\n", hash_name(&hyper.source)).as_bytes())?;
-                } else {
-                    output.write(
-                        format!("h{}[shape=none,label=\"\",width=0,height=0];\n", hyper_idx)
+        for edge in graph.succ(&vertex) {
+            // TODO maybe print labels on edges
+            match edge {
+                Edges::HYPER(hyper) => {
+                    // TODO create a direct arrow when the hyper-edge only contains one target
+                    if hyper.targets.is_empty() {
+                        output
+                            .write(format!("v{} -> ∅;\n", hash_name(&hyper.source)).as_bytes())?;
+                    } else {
+                        output.write(
+                            format!("h{}[shape=none,label=\"\",width=0,height=0];\n", hyper_idx)
+                                .as_bytes(),
+                        )?;
+                        output.write(
+                            format!(
+                                "v{} -> h{}[dir=none];\n",
+                                hash_name(&hyper.source),
+                                hyper_idx
+                            )
                             .as_bytes(),
-                    )?;
+                        )?;
+                        for target in hyper.targets {
+                            output.write(
+                                format!("h{} -> v{};\n", hyper_idx, hash_name(&target)).as_bytes(),
+                            )?;
+
+                            if !visited.contains(&target) {
+                                print_vertex(&target, &mut output);
+                                queue.push_back(target.clone());
+                                visited.insert(target);
+                            }
+                        }
+                        hyper_idx += 1;
+                    }
+                }
+                Edges::NEGATION(neg) => {
                     output.write(
                         format!(
-                            "v{} -> h{}[dir=none];\n",
-                            hash_name(&hyper.source),
-                            hyper_idx
+                            "v{} -> v{}[style=dashed];\n",
+                            hash_name(&neg.source),
+                            hash_name(&neg.target)
                         )
                         .as_bytes(),
                     )?;
-                    for target in hyper.targets {
-                        output.write(
-                            format!("h{} -> v{};\n", hash_name(&hyper.source), hyper_idx)
-                                .as_bytes(),
-                        )?;
 
-                        if !visited.contains(&target) {
-                            print_vertex(&target, &mut output);
-                            visited.insert(target);
-                        }
+                    if !visited.contains(&neg.target) {
+                        print_vertex(&neg.target, &mut output);
+                        queue.push_back(neg.target.clone());
+                        visited.insert(neg.target);
                     }
-                    hyper_idx += 1;
                 }
-            }
-            Edges::NEGATION(neg) => {
-                output.write(
-                    format!(
-                        "v{} -> v{}[style=dashed];\n",
-                        hash_name(&neg.source),
-                        hash_name(&neg.target)
-                    )
-                    .as_bytes(),
-                )?;
-
-                if !visited.contains(&neg.target) {
-                    print_vertex(&neg.target, &mut output);
-                    visited.insert(neg.target);
-                }
-            }
-        };
+            };
+        }
     }
 
     output.write("}\n".as_bytes())?;
