4700e81a588dd769230a2ef513bf3cf737600b2b
NicEastvillage <nicoesterby@gmail.com>
2021-03-24 15:46:17 +0100

    Added index translation.

--- atl-checker/src/atl/gamestructure/mod.rs
+++ atl-checker/src/atl/gamestructure/mod.rs
@@ -3,7 +3,8 @@ use std::collections::HashSet;
 #[allow(unused_imports)]
 pub use eager::EagerGameStructure;
 
-use crate::atl::common::{Proposition, State};
+use crate::atl::common::{Action, Player, Proposition, State};
+use std::fmt::Formatter;
 
 mod eager;
 
@@ -16,4 +17,16 @@ pub trait GameStructure {
 
     /// Returns the number of moves each player can take when the game is in `state`.
     fn move_count(&self, state: State) -> Vec<usize>;
+
+    /// Returns the human-readable name of the given state
+    fn state_name(&self, state: State) -> String;
+
+    /// Returns the human-readable name of the given proposition
+    fn label_name(&self, proposition: Proposition) -> String;
+
+    /// Returns the human-readable name of the given player
+    fn player_name(&self, player: Player) -> String;
+
+    /// Returns the human-readable name of the given action by the given player in the given state
+    fn action_name(&self, state: State, player: Player, action: Action) -> String;
 }
